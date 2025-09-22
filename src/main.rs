051cf8a54a94ddf197855c795298592ebbd19a7d
khun <khunor22@gmail.com>
2025-09-22 19:00:22 +0200

    First version done
    currently writes result to the console and puts needless media/before
    every file. TODO: Make easy to use release

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::{fs::File, path::PathBuf};
+
+use glob::glob;
+
+struct Info {
+    name: String,
+    len: String,
+}
+
+impl Info {
+    fn new() -> Self {
+        Self {
+            name: String::new(),
+            len: String::new(),
+        }
+    }
+}
+
+fn get_all_files() -> glob::Paths {
+    glob("./media/*").expect("Failed to read files")
+}
+
+fn read_mp4(path: PathBuf) -> Info {
+    let f = File::open(path.clone()).unwrap();
+    let mp4 = mp4::read_mp4(f).unwrap();
+    let mut res = Info::new();
+    for track in mp4.tracks().values() {
+        res = Info {
+            name: String::from(path.to_str().unwrap()),
+            len: track.duration().as_secs().to_string(),
+        };
+    }
+    res
+}
+
 fn main() {
-    println!("Hello, world!");
+    let files = get_all_files();
+    let mut output = Vec::new();
+    for entry in files {
+        match entry {
+            Ok(path) => output.push(read_mp4(path)),
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    for info in output {
+        println!("{}\t{}", info.name, info.len)
+    }
 }
